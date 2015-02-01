b1804be1f317dbc365fc60508c05312761ed5e33
Hendrik Mans <hendrik@mans.de>
2015-02-01 10:57:44 +0100

    Disable Rails generators we don’t need

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # We don't need these generators
+  config.generators.stylesheets = false
+  config.generators.javascripts = false
+  config.generators.helper      = false
 end
