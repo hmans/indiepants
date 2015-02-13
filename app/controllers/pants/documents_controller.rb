f46dedf6f5e44206a281670f32a470f28e4241da
Hendrik Mans <hendrik@mans.de>
2015-02-13 14:21:06 +0100

    DocumentsController -> Pants::DocumentsController

--- app/controllers/pants/documents_controller.rb
+++ app/controllers/pants/documents_controller.rb
@@ -1,4 +1,4 @@
-class DocumentsController < ApplicationController
+class Pants::DocumentsController < ApplicationController
   respond_to :html, :json
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
-  scope :pants do
+  namespace :pants do
     resources :documents
     resources :webmentions
 
     # Setup
-    get  'setup' => 'pants/setup#setup'
-    post 'setup' => 'pants/setup#setup'
+    get  'setup' => 'setup#setup'
+    post 'setup' => 'setup#setup'
 
     # Authentication
-    match  'login'  => 'pants/auth#login', via: [:get, :post]
-    delete 'login' => 'pants/auth#logout'
+    match  'login' => 'auth#login', via: [:get, :post]
+    delete 'login' => 'auth#logout'
   end
 
   # Posts
-  get ':year/:month/:day/:slug' => 'documents#show', as: 'nice_document'
-  get ':id' => 'documents#show', as: 'uid'
+  get ':year/:month/:day/:slug' => 'pants/documents#show', as: 'nice_document'
+  get ':id' => 'pants/documents#show', as: 'uid'
 
-  root to: "documents#index"
+  root to: "pants/documents#index"
 end
