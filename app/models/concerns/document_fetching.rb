ffc1fed388f7a02c0e1c3e4563c0b3f0bef91e99
Hendrik Mans <hendrik@mans.de>
2015-02-13 16:18:21 +0100

    Actually consume pants-document JSON when available.

--- app/models/concerns/document_fetching.rb
+++ app/models/concerns/document_fetching.rb
@@ -2,31 +2,36 @@ module DocumentFetching
   extend ActiveSupport::Concern
 
   def fetch!
+    # Check if we have a URL
+    raise "no URL given" if url.blank?
+
+    # Abort if we're local (no need to fetch)
     return false if local?
 
-    # Let's analyze the page a little.
+    # Load and parse the page
     doc = HTTParty.get(url).to_s
     page = Nokogiri::HTML(doc)
 
     fetch_from_pants_json(page) ||
       fetch_from_microformats(page) ||
       fetch_from_magic_extraction(page)
-
-    # Success!
-    true
   end
 
   def fetch_from_pants_json(page)
-    # if pants_json_url = page.css("head>link[rel=pants]").first.try(:[], "href")
-    #   # TODO: fetch JSON and copy it into this record
-    # end
+    if link = page.css("head>link[rel=pants-document]")
+      if pants_json_url = link.first.try(:[], "href")
+        json = HTTParty.get(URI.join(url, pants_json_url))
+        consume_json(json)
+        :pants
+      end
+    end
   end
 
   def fetch_from_microformats(page)
     if h_entry = page.css('.h-entry').first
       self.html = h_entry.css('.e-content').children.to_s rescue nil
       self.published_at = h_entry.css('.dt-published').attr('datetime').to_s rescue nil
-      true
+      :microformats
     end
   end
 
@@ -38,6 +43,17 @@ module DocumentFetching
   #
   def fetch?
     new_record? || (remote? && updated_at < 30.minutes.ago)
+  end
+
+  def consume_json(json)
+    # If this document already has a URL, check if it's equal to the URL
+    # given in the JSON.
+    raise "URL mismatch" if url.present? && json["url"] != url
+
+    # Copy over the attributes that we consider safe.
+    allowed_attributes = %w[url type title html data tags previous_urls published_at]
+    self.attributes = json.slice(*allowed_attributes)
+    self
   end
 
   module ClassMethods
