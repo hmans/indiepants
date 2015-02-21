561dfd16fc81d0ad57572e6694e448311ac78f27
Hendrik Mans <hendrik@mans.de>
2015-02-21 15:28:42 +0100

    Remove obsolete User#webmentions relation

--- app/models/pants/user.rb
+++ app/models/pants/user.rb
@@ -10,9 +10,6 @@ module Pants
     has_many :documents,
       dependent: :destroy
 
-    has_many :webmentions,
-      dependent: :destroy
-
     scope :local,  -> { where(local: true) }
     scope :remote, -> { where(local: false) }
 
