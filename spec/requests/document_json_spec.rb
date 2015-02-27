443398f417ffbe822659a6b6d5d525ab74b62a25
Hendrik Mans <hendrik@mans.de>
2015-02-27 22:16:53 +0100

    Add a spec for /pants/documents.json

--- spec/requests/document_json_spec.rb
+++ spec/requests/document_json_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "requesting a document's JSON" do
+describe "GET /pants/documents/:id.json" do
   let!(:document) { create :document }
 
   specify do
@@ -10,5 +10,20 @@ describe "requesting a document's JSON" do
     expect(response.content_type).to eq('application/json')
 
     expect(json).to eq(expected_document_json(document))
+  end
+end
+
+describe "GET /pants/documents.json" do
+  let!(:user) { create :user }
+  let!(:documents) { create_list :document, 10, user: user }
+
+  specify do
+    get pants_documents_url(format: 'json', host: user.host)
+
+    expect(response).to be_success
+    expect(response.content_type).to eq('application/json')
+
+    expect(json['total']).to eq(10)
+    expect(json['documents']).to eq(user.documents.map { |d| expected_document_json(d) })
   end
 end
