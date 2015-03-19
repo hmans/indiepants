11d532b13b24b3f61c519b481a25c483a10168c5
Hendrik Mans <hendrik@mans.de>
2015-03-19 05:50:13 +0100

    Flesh out document deduplication specs.

--- spec/factories/documents.rb
+++ spec/factories/documents.rb
@@ -3,5 +3,16 @@ FactoryGirl.define do
     user
     html "<p>Lorem Ipsum.</p>"
     sequence(:path) { |i| "/document-#{i}" }
+
+    factory :remote_document do
+      user { build(:remote_user) }
+    end
+
+    trait :with_stubs do
+      after(:build) do |document|
+        WebMock.stub_request :get, document.user.url
+        WebMock.stub_request :get, document.url
+      end
+    end
   end
 end
--- spec/models/pants/document_deduplication_spec.rb
+++ spec/models/pants/document_deduplication_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 describe Pants::Document do
-  let(:original) { build :document }
+  let(:original) { build :remote_document, :with_stubs }
 
   context "with a UID" do
     before { original.uid = 'http://my/uid' }
 
     context "when there is another document with the same UID" do
-      let!(:duplicate) { create :document, user: original.user, uid: original.uid }
+      let!(:duplicate) do
+        create :remote_document, :with_stubs, user: original.user, uid: original.uid
+      end
 
       describe '#duplicates' do
         it "returns duplicate posts" do
@@ -15,9 +17,10 @@ describe Pants::Document do
         end
       end
 
-      xit "removes the other document" do
+      it "removes the other document" do
         expect { original.save! }
-          .to change { duplicate.destroyed? }
+          .to change { Pants::Document.where(id: duplicate.id).first }
+          .from(duplicate).to(nil)
       end
     end
   end
@@ -26,11 +29,38 @@ describe Pants::Document do
     before { original.uid = nil }
 
     context "when there is another document with the same path" do
-      it "removes the other document"
+      let!(:duplicate) do
+        create :remote_document, :with_stubs, user: original.user, path: original.path
+      end
+
+      describe '#duplicates' do
+        it "returns duplicate posts" do
+          expect(original.duplicates).to eq([duplicate])
+        end
+      end
+
+      it "removes the other document" do
+        expect { original.save! }
+          .to change { Pants::Document.where(id: duplicate.id).first }
+          .from(duplicate).to(nil)
+      end
     end
 
     context "when there is no other document with the same path" do
-      it "does nothing"
+      let!(:duplicate) do
+        create :remote_document, :with_stubs, user: original.user
+      end
+
+      describe '#duplicates' do
+        it "returns nothing" do
+          expect(original.duplicates).to eq([])
+        end
+      end
+
+      it "removes the other document" do
+        expect { original.save! }
+          .to_not change { Pants::Document.where(id: duplicate.id).first }
+      end
     end
   end
 end
