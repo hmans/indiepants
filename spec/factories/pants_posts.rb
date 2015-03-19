58178072128b82836900912dd5e0e16a08b7c60d
Hendrik Mans <hendrik@mans.de>
2015-03-19 05:18:41 +0100

    Update dependencies

--- spec/factories/pants_posts.rb
+++ spec/factories/pants_posts.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
   factory :pants_post, :class => 'Pants::Post', parent: :document do
-    body { Faker::Lorem.paragraphs(rand(1..3)).join("\n\n") }
+    body { FFaker::Lorem.paragraphs(rand(1..3)).join("\n\n") }
   end
 end
